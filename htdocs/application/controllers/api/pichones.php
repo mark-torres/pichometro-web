18f6f79e989f222f9b5ac26875bd48be3fb8c4c9
Mark Torres <mark.torres.mets@gmail.com>
2016-08-28 19:23:50 -0500

    Add codeigniter 2

---
+++ htdocs/application/controllers/api/pichones.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pichones extends CI_Controller {
+	private $response;
+
+	public function __construct() {
+		parent::__construct();
+		// API default values
+		$this->response = array(
+			'success' => false,
+			'message' => "Nothing done"
+		);
+		if (!empty($_POST)) {
+			$this->response['post'] = $_POST;
+		}
+	}
+
+	private function write_response() {
+		header("Content-Type: application/json");
+		die(json_encode($this->response));
+	}
+
+	// - - - - - - - - - - - - - -
+
+	public function create() {
+		$this->write_response();
+	}
+
+	public function save() {
+		$this->write_response();
+	}
+
+	public function delete() {
+		$this->write_response();
+	}
+
+	public function show() {
+		$this->write_response();
+	}
+
+}
