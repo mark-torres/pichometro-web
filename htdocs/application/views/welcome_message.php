c928eba22236abe2e7a56c7daa24d3bc64fc3e7d
Mark Torres <mark.torres.mets@gmail.com>
2016-09-06 22:04:55 -0500

    Plantilla con bootstrap

--- htdocs/application/views/welcome_message.php
+++ htdocs/application/views/welcome_message.php
@@ -14,16 +14,181 @@
 	<title>El Pich&oacute;metro</title>
 </head>
 <body class="container">
-	<nav class="navbar navbar-default">
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+		<!-- Mobile -->
 		<div class="nav-header">
-			
+			<button type="button" class="navbar-toggle collapsed" 
+				data-toggle="collapse" data-target="#mainNavBar" aria-expanded="false">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="" alt="logo" width="40" height="40"></img>
+			</a>
+		</div>
+		<!-- Nav elements -->
+		<div class="collapse navbar-collapse" id="mainNavBar">
+			<ul class="nav navbar-nav navbar-left">
+				<li>
+					<a href="#">Pich&oacute;metros</a>
+				</li>
+				<li>
+					<a href="#">Pichones</a>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li>
+					<a href="#">Entrar</a>
+				</li>
+				<li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+			</ul>
 		</div>
 	</nav>
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
 	<div class="jumbotron">
 		<h1>
 			El Pich&oacute;metro
 		</h1>
+		<p>
+			Cras mattis consectetur purus sit amet fermentum. 
+			Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+		</p>
+		<p>
+			<a class="btn btn-primary btn-lg" href="#">Reg&iacute;strate</a>
+		</p>
 	</div>
+	
+	<div class="page-header">
+		<h1>
+			&iquest;Qui&eacute;n es el m&aacute;s pich&oacute;n?
+		</h1>
+	</div>
+	
+	<p>
+		Esta es la lista de los pich&oacute;metros m&aacute;s populares y sus respectivos pichones:
+	</p>
+	
+	<div class="row">
+		<div class="col-md-3">
+			<h3>
+				Tortor Fringilla
+			</h3>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="1ro"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Malesuada</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Nibh</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Elit</h4>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<h3>
+				Consectetur Inceptos
+			</h3>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="1ro"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Justo</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Ipsum</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Euismod</h4>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<h3>
+				Adipiscing Etiam
+			</h3>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="1ro"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Purus</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Mattis</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Malesuada</h4>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-3">
+			<h3>
+				Ridiculus Sollicitudin
+			</h3>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="1ro"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Pharetra</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Elit</h4>
+				</div>
+			</div>
+			<div class="media">
+				<div class="media-left">
+					<img class="media-object" src="" alt="2do"/ width="48" height="48">
+				</div>
+				<div class="media-body">
+					<h4>Fringilla</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 	<p>&nbsp;</p>
 </body>
 </html>
